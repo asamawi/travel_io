49aa178365e34eb832a01b4defef587058261765
Ahmad Samawi <asamawi@n4s-solutions.com>
2022-02-14 19:58:20 +0300

    Update hooks.py

--- travel_io/hooks.py
+++ travel_io/hooks.py
@@ -18,7 +18,8 @@ app_license = "MIT"
 
 # include js, css files in header of web template
 web_include_css = ["/assets/travel_io/css/travel_io.css", "/assets/travel_io/css/slick.css", "/assets/travel_io/css/slick-theme.css"]
-web_include_js = ["/assets/travel_io/js/travel_io.js", "/assets/travel_io/js/jquery.scrollTo.min.js", "/assets/travel_io/js/jquery.singlePageNav.min.js", "/assets/travel_io/js/slick.min.js"]
+web_include_js = ["/assets/travel_io/js/travel_io.js", "/assets/travel_io/js/jquery.scrollTo.min.js", "/assets/travel_io/js/jquery.singlePageNav.min.js",
+		  "/assets/travel_io/js/slick.min.js", "/assets/travel_io/js/popper.min.js"]
 
 # include custom scss in every website theme (without file extension ".scss")
 # website_theme_scss = "travel_io/public/scss/website"
